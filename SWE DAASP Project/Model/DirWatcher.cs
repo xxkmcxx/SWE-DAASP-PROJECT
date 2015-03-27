f28a48490ae08fd66cf6d6e18147ed9bfb638ef6
Yoshuam A. Alicea Casillas <yoshuamsteps@gmail.com>
2015-03-27 18:01:20 -0300

    Branch Created

---
+++ SWE DAASP Project/Model/DirWatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace SWE_DAASP_Project.Model
+{
+    class DirWatcher
+    {
+        
+
+        public static void Run()
+        {
+            string path = "C:/Users/Yoshuam Alicea/Desktop/TestDir";      
+            FileSystemWatcher watcher = new FileSystemWatcher(path);
+
+            //watcher.Path = path;
+            watcher.Filter = "*.*";
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.CreationTime | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnDeleted);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+        }
+
+        private static void OnRenamed(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
---
+++ Watcher/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace Watcher
+{
+    class Program
+    {
+       static int KEY;
+       static string new_path;
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            do
+            {
+
+                string path = @"C:\Users\Yoshuam Alicea\Desktop\TestDir";
+                FileSystemWatcher watcher = new FileSystemWatcher(path);
+
+                watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                watcher.Filter = "*.*";
+
+                watcher.Created += new FileSystemEventHandler(OnCreated);
+                watcher.Renamed += new RenamedEventHandler(OnRenamed);
+                watcher.Deleted += new FileSystemEventHandler(OnDelete);
+
+                watcher.EnableRaisingEvents = true;
+
+               if(KEY == 1)
+               {
+                    
+                    KEY = 0;
+               }
+                        
+                    
+
+                Console.WriteLine("Press q to quit program");
+               
+            } while (Console.Read() != 'q');
+        }
+
+        private static void OnDelete(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: " + e.Name + " Deleted");
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine("File Renamed From " + e.OldName + " to " + e.Name);
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File Created: " + e.FullPath);
+            var excelApp = new Excel.Application();
+            excelApp.Visible = true;
+
+            Excel.Workbooks books = excelApp.Workbooks;
+            Excel.Workbook sheet = books.Open(e.FullPath);            
+        }
+       
+    }
+}
